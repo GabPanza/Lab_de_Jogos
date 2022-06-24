fa760430456807002cc614a588853e494b8eb762
GabPanza <gabrieljp@id.uff.br>
2022-06-24 16:12:48 -0300

    Update

--- 3- Final Project/main.py
+++ 3- Final Project/main.py
@@ -9,6 +9,7 @@ from PPlay.mouse import*
 from PPlay.sound import*
 import menu
 import Player
+import enemy
 import shooting
 import pygame
 from pygame import mixer
@@ -31,19 +32,32 @@ def game(vidas,vidasInimigo,movimento,movimentoInimigo,velProjetil,velProjetilIn
     
     # Instancio os cenários
     cenarioFloresta = GameImage("Floresta.jpg")
-    #cenarioFloresta2 = GameImage("Floresta2.jpg")
+    chaoFloresta = Sprite("FlorestaChao.png",1)
+    chaoFloresta.y = janela.height-chaoFloresta.height
+    
+    cenarioFloresta2 = GameImage("Floresta2.jpg")
+    chaoFloresta2 = Sprite("FlorestaChao2.png",1)
+    chaoFloresta2.y = janela.height-chaoFloresta2.height
+    
     cenarioCastelo = GameImage("Castelo.jpg")
-    #cenarioCastelo2 = GameImage("Castelo2.jpg")
+    chaoCastelo = Sprite("CasteloChao.png",1)
+    chaoCastelo.y = janela.height-chaoCastelo.height
+
+    cenarioCastelo2 = GameImage("Castelo2.jpg")
+    chaoCastelo2 = Sprite("CasteloChao2.png",1)
+    chaoCastelo2.y = janela.height-chaoCastelo2.height
+    
     cenarioDungeon = GameImage("Dungeon.jpg")
+    chaoDungeon = Sprite("DungeonChao.png",1) 
+    chaoDungeon.y = janela.height-chaoDungeon.height
+    
     cenario = 1
+    chao = chaoFloresta
     
     # Instancio os objetos
     placa = Sprite("Placa.png",1)
     placa.x = janela.width - placa.width - 50
     placa.y = janela.height-placa.height-240    
-    chaoFloresta = Sprite("FlorestaChao.png",1)
-    chaoFloresta.y = janela.height-chaoFloresta.height
-    
     plataforma = GameImage("Plataforma.jpg")
     
     # Instancio a Emih
@@ -196,24 +210,84 @@ def game(vidas,vidasInimigo,movimento,movimentoInimigo,velProjetil,velProjetilIn
     ##################################################### Game Loop / Update() #####################################################
     ################################################################################################################################
     while(True):
+        # Defino o Framerate
+        clock.tick(FPS)
+        
         # Desenho o cenario desejado e a placa que indica o que precisa ser feito
         if cenario==1:
             cenarioFloresta.draw()
             placa.draw()
+            # Crio a movimentacao do minotauro
+            checkPosInim = enemy.moveEnemy(janela,player,movimento,chao,checkPosInim)
+            if checkPosInim==0:
+                if (minotauro.x>player.x and minotauro.x<janela.width):
+                    minotauro = enemy.SetEnemy(minotauro_Dir_Run,minotauro)
+                    minotauro.update()
+                if not player:
+                    minotauro = enemy.SetEnemy(minotauro_Dir,minotauro)
+            elif checkPosInim==1:
+                if (minotauro.x<player.x and minotauro.x>0):
+                    minotauro = enemy.SetEnemy(minotauro_Esq_Run,minotauro)
+                    minotauro.update()
+                if not player:
+                    minotauro = enemy.SetEnemy(minotauro_Esq,minotauro)
+        
         elif cenario==2:
-            #cenarioFloresta2.draw()
+            cenarioFloresta2.draw()
             placa.draw()
+        
         elif cenario==3:
             cenarioCastelo.draw()
             placa.draw()
+            # Crio a movimentacao do guarda
+            checkPosInim = enemy.moveEnemy(janela,player,movimento,chao,checkPosInim)
+            if checkPosInim==0:
+                if (guardas.x>player.x and guardas.x<janela.width):
+                    guardas = enemy.SetEnemy(guardas_Dir_Run,guardas)
+                    guardas.update()
+                if not player:
+                    guardas = enemy.SetEnemy(guardas_Dir,guardas)
+            elif checkPosInim==1:
+                if (guardas.x<player.x and guardas.x>0):
+                    guardas = enemy.SetEnemy(guardas_Esq_Run,guardas)
+                    guardas.update()
+                if not player:
+                    guardas = enemy.SetEnemy(guardas_Esq,guardas)
+        
         elif cenario==4:
-            #cenarioCastelo2.draw()
+            cenarioCastelo2.draw()
             placa.draw()
+            # Crio a movimentacao do cultista
+            checkPosInim = enemy.moveEnemy(janela,player,movimento,chao,checkPosInim)
+            if checkPosInim==0:
+                if (cultista.x>player.x and cultista.x<janela.width):
+                    cultista = enemy.SetEnemy(cultista_Dir_Run,cultista)
+                    cultista.update()
+                if not player:
+                    cultista = enemy.SetEnemy(cultista_Dir,cultista)
+            elif checkPosInim==1:
+                if (cultista.x<player.x and cultista.x>0):
+                    cultista = enemy.SetEnemy(cultista_Esq_Run,cultista)
+                    cultista.update()
+                if not player:
+                    cultista = enemy.SetEnemy(cultista_Esq,cultista)
+        
         elif cenario==5:
             cenarioDungeon.draw()
-        
-        # Defino o Framerate
-        clock.tick(FPS)
+            # Crio a movimentacao do Caebralum
+            checkPosInim = enemy.moveEnemy(janela,player,movimento,chao,checkPosInim)
+            if checkPosInim==0:
+                if (caebralum.x>player.x and caebralum.x<janela.width):
+                    caebralum = enemy.SetEnemy(caebralum_Dir_Run,caebralum)
+                    caebralum.update()
+                if not player:
+                    caebralum = enemy.SetEnemy(caebralum_Dir,caebralum)
+            elif checkPosInim==1:
+                if (caebralum.x<player.x and caebralum.x>0):
+                    caebralum = enemy.SetEnemy(caebralum_Esq_Run,caebralum)
+                    caebralum.update()
+                if not player:
+                    caebralum = enemy.SetEnemy(caebralum_Esq,caebralum)
         
         # Volto pro menu
         if (teclado.key_pressed("ESC")):
@@ -224,7 +298,7 @@ def game(vidas,vidasInimigo,movimento,movimentoInimigo,velProjetil,velProjetilIn
         life(vidas)
         
         # Crio a movimentacao do personagem principal
-        checkPos = Player.movePlayer(janela,teclado,player,movimento,chaoFloresta,checkPos)
+        checkPos = Player.movePlayer(janela,teclado,player,movimento,chao,checkPos)
         if checkPos==0:
             if (teclado.key_pressed("D") or teclado.key_pressed("RIGHT")):
                 player = Player.SetPlayer(player_Dir_Run,player)
@@ -237,7 +311,7 @@ def game(vidas,vidasInimigo,movimento,movimentoInimigo,velProjetil,velProjetilIn
                 player.update()
             else:
                 player = Player.SetPlayer(player_Esq,player)
-
+        
         # Crio a condiçao para mudar de cenário
         if ((player.x>janela.width-player.width/2) and (len(listaInimigos) == 0)):
             cenario+=1
